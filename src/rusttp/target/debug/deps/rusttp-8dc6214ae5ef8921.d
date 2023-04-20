e7987c507a4f0142c989d55f49ebb3c31fecf1d6
Celeste <58823378+Autumnial@users.noreply.github.com>
2023-04-20 21:35:14 +0200

    did a silly little oopsie :p

--- src/rusttp/target/debug/deps/rusttp-8dc6214ae5ef8921.d
+++
@@ -1,5 +0,0 @@
-C:\dev\rust\http\src\rusttp\target\debug\deps\rusttp-8dc6214ae5ef8921.rmeta: src\lib.rs
-
-C:\dev\rust\http\src\rusttp\target\debug\deps\rusttp-8dc6214ae5ef8921.d: src\lib.rs
-
-src\lib.rs:
